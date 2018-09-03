af8d6f2d9e0655e116d29f4a27fb4852b496d751
reactexcel <arun.etech2011@gmail.com>
2018-09-03 15:22:36 +0530

    single heiracrchy add delete done

--- src/components/BulletList.js
+++ src/components/BulletList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import _ from 'lodash'
+import ReactDOM from 'react-dom';
 
 class BulletList extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
+			timesBackspacePressed: 0,
 			item_selected_for_edit: 0,
 			list: []
 		}
@@ -16,13 +18,11 @@ class BulletList extends Component {
 		})
 	}
 
-	_onChangeItemText = (e) => {
-		let editedItemId = e.target.id
-		let editedItemValue = e.target.value
+	_updateItem = ( itemid, value ) => {
 		let newList = _.map( this.state.list, (item, id) => {
-			if( id == editedItemId ){
+			if( id == itemid ){
 				return {
-					text: editedItemValue
+					text: value
 				}
 			} else {
 				return item
@@ -33,23 +33,83 @@ class BulletList extends Component {
 		})
 	}
 
-	_renderSelectedItem = ( id, item ) => {
+	_deleteItem = ( itemid ) => {
+		let newList = this.state.list;
+		_.remove( newList, (item, id) => {
+			if( id == itemid ){
+				return true
+			}
+		})
+		let toBeFocusLi = itemid - 1;
+		if( toBeFocusLi < 0 ){
+			toBeFocusLi = 0;
+		}
+		this.setState({
+			list: newList,
+			timesBackspacePressed: 0 // reset
+		},() => {
+			this._selectItemForEdit( toBeFocusLi )
+		})
+	}
+
+	_onChangeItemText = (e) => {
+		let editedItemId = e.target.id
+		let editedItemValue = e.target.value
+		this._updateItem( editedItemId, editedItemValue );		
+	}
+
+	_addEmptyNewListItem = ( parentUL_key ) => {
+		let newList = this.state.list;
+		newList.push({
+			text: ''
+		})
+		let toBeFocusLi = newList.length - 1;
+		this.setState({
+			list: newList
+		},() => {
+			this._selectItemForEdit( toBeFocusLi )
+		})
+	}
+
+	_onKeyPressItemText = (e) => {
+		if( e.key == 'Enter' ){
+			let editedItemId = e.target.id
+			let parentULid = e.target.getAttribute('data-set-id');
+			let parentUL_key = parentULid.replace("UL", "").trim();
+			this._addEmptyNewListItem( parentUL_key );
+		} else if( e.key == 'Backspace' ){
+			let timesBackspacePressed = this.state.timesBackspacePressed;
+			if( e.target.value.length == 0 ) {
+				timesBackspacePressed += 1;
+			}
+			if( timesBackspacePressed == 2 ){
+				this._deleteItem( e.target.id )
+			} else {
+				this.setState({ timesBackspacePressed: timesBackspacePressed })
+			}
+		}
+	}
+
+	_renderSelectedItem = ( ULid, id, item ) => {
 		return <li id={id} key={id}>
 			<input
+				autoFocus
 				id={id}
 				className="current-edit-item" 
+				data-set-id={ULid}
 				type="text" 
 				value={item.text}
 				onChange={this._onChangeItemText}
+				onKeyDown={this._onKeyPressItemText}
 			/>
 		</li>
 	}
 
-	_renderListItem = ( id, item ) => {
+	_renderListItem = ( ULid, id, item ) => {
 		if( id === this.state.item_selected_for_edit ){
-			return this._renderSelectedItem( id, item )
+			return this._renderSelectedItem( ULid, id, item )
 		} else {			
-			return <li id={id} key={id} onClick={() => this._selectItemForEdit(id)}>
+			return <li id={id} key={id} data-set-id={ULid} onClick={() => this._selectItemForEdit(id)}>
 				{item.text}
 			</li>
 		}
@@ -62,16 +122,17 @@ class BulletList extends Component {
 				text: ""
 			})
 		}
-		console.log( list )
-		return <ul>
+		// console.log( list )
+		let ULid = "UL0";
+		return <ul id={ULid}>
 			{_.map(list, (item, id ) => {
-				return this._renderListItem(id, item)
+				return this._renderListItem(ULid, id, item)
 			})}
 		</ul>
 	}
 
   render() {
-  	console.log( this.state )
+  	// console.log( this.state )
   	let listItems = this._renderList();
     return (
     	<div id="bullets-list">
